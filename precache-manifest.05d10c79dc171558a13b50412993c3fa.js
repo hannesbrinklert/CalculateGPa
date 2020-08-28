08b8f837d9489a37e80b5fe1a388897fdf46baf1
Hannes Brinklert <hannes@brinklert.se>
2020-08-28 19:18:34 +0200

    Updates

---
+++ precache-manifest.05d10c79dc171558a13b50412993c3fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c0668c6d6c7f91f73fe98e31d948bc",
+    "url": "/CalculateGPa/index.html"
+  },
+  {
+    "revision": "2e69cd5f0aa4392d8304",
+    "url": "/CalculateGPa/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5efb0f67948dc3a5579b",
+    "url": "/CalculateGPa/static/css/main.36a2efdc.chunk.css"
+  },
+  {
+    "revision": "2e69cd5f0aa4392d8304",
+    "url": "/CalculateGPa/static/js/2.7e9bf640.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CalculateGPa/static/js/2.7e9bf640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5efb0f67948dc3a5579b",
+    "url": "/CalculateGPa/static/js/main.8233953b.chunk.js"
+  },
+  {
+    "revision": "483dd52b8df22e034cf0",
+    "url": "/CalculateGPa/static/js/runtime-main.37bac062.js"
+  }
+]);
